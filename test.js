3d26c284c853f90f273e9679e08123841acc2ed7
annTerry <annak@1gb.ru>
2023-01-17 20:49:57 +0300

    feat: solution

--- test.js
+++ test.js
@@ -47,7 +47,7 @@ it('should check if brackets sequence is correct 9', () => {
 
 it('should check if brackets sequence is not correct 10', () => {
   assert.equal(check('|(|)', config5), false);
-}); 
+});
 
 it('should check if brackets sequence is correct 11', () => {
   assert.equal(check('|()|(||)||', config5), true);
@@ -87,4 +87,4 @@ it('should check if brackets sequence is correct 19', () => {
 
 it('should check if brackets sequence is correct 20', () => {
   assert.equal(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))', config7), true);
-}); 
+});
