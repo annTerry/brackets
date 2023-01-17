afad31575632978bf5a25ad322c518b08b0772ea
annTerry <annak@1gb.ru>
2023-01-17 20:49:24 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const stack = [];
+  const comfortConfig = {};
+
+  bracketsConfig.forEach((oneConfig, index) => {
+    if(oneConfig[0] !== oneConfig[1] ) {
+    comfortConfig[oneConfig[0]] = [0,index];
+    comfortConfig[oneConfig[1]] = [1,index];
+    }
+    else {
+      comfortConfig[oneConfig[0]] = [2, index]; 
+    }
+  });
+
+  const strArray = str.split('');
+  for (let char of strArray) {
+
+    const bracketData = comfortConfig[char];
+
+    if (bracketData) {
+      if ((stack.length === 0) || (bracketData[0] === 0)) {
+        stack.push(bracketData);
+      }
+      else if (bracketData[0] === 2) {
+        let checkStack = stack[stack.length - 1];
+        if (checkStack[0] === 2 && checkStack[1] === bracketData[1]) {
+          stack.pop();
+        }
+        else {
+          stack.push(bracketData);
+        }
+      }
+      else {
+        const lastData = stack.pop();
+        if (lastData[1] !== bracketData[1]) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
